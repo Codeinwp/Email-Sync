855edb987f85e150ee72e17231b8cfafba7d2b6c
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-05-08 11:02:19 +0100

    Revert "Change to abstract class"
    This reverts commit 826a0a49db696643e0d67890042ed11f124d9a62.

--- src/Integration.php
+++ src/Integration.php
@@ -1,45 +1,17 @@
 <?php namespace Dev7EmailSync;
 
-abstract class Integration {
+interface Integration {
 
-	abstract public function is_subscribed( $user_id );
+	public function is_subscribed( $user_id );
 
-	abstract public function subscribe( $user_id );
+	public function subscribe( $user_id );
 
-	abstract public function update_subscription( $user_id );
+	public function update_subscription( $user_id );
 
-	abstract public function unsubscribe( $user_id );
+	public function unsubscribe( $user_id );
 
-	public function settings_init()
-	{
+	public function settings_init();
 
-	}
-
-	public function sanitize( $input )
-	{
-		return $input;
-	}
-
-	protected function get( $url, $args = array() )
-	{
-		$response = wp_remote_get( $url, $args );
-
-		if ( !is_wp_error( $response ) ) {
-			return wp_remote_retrieve_body( $response );
-		}
-
-		return null;
-	}
-
-	protected function post( $url, $args = array() )
-	{
-		$response = wp_remote_post( $url, $args );
-
-		if ( !is_wp_error( $response ) ) {
-			return wp_remote_retrieve_body( $response );
-		}
-
-		return null;
-	}
+	public function sanitize( $input );
 
 }
